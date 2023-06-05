b83199d2371a5e70134810ac77484dd7b8a7f6c9
unpopularcoder <unpopularcoder@proton.me>
2023-06-05 13:21:21 +0000

    organization

--- backend/models/trade.go
+++ backend/models/trade.go
@@ -31,3 +31,86 @@ type Trade struct {
 	TakerSide       string          `json:"takerSide"        db:"taker_side"`
 	MakerOrderID    string          `json:"makerOrderID"     db:"maker_order_id"`
 	TakerOrderID    string          `json:"takerOrderID"     db:"taker_order_id"`
+	Sequence        int             `json:"sequence"         db:"sequence"`
+	Amount          decimal.Decimal `json:"amount"           db:"amount"`
+	Price           decimal.Decimal `json:"price"            db:"price"`
+	ExecutedAt      time.Time       `json:"executedAt"       db:"executed_at"`
+	CreatedAt       time.Time       `json:"createdAt"        db:"created_at"`
+	UpdatedAt       time.Time       `json:"updatedAt"        db:"updated_at"`
+}
+
+func (Trade) TableName() string {
+	return "trades"
+}
+
+var TradeDao ITradeDao
+var TradeDaoPG ITradeDao
+
+func init() {
+	TradeDao = &tradeDaoPG{}
+	TradeDaoPG = TradeDao
+}
+
+type tradeDaoPG struct {
+}
+
+func (tradeDaoPG) FindTradesByMarket(marketID string, startTime time.Time, endTime time.Time) []*Trade {
+	var trades []*Trade
+
+	DB.Where("market_id = ? and status = ? and executed_at between ? and ? ", marketID, common.STATUS_SUCCESSFUL, startTime, endTime).Order("executed_at desc").Find(&trades)
+	return trades
+}
+
+func (tradeDaoPG) FindAllTrades(marketID string) (int64, []*Trade) {
+	var trades []*Trade
+	var count int64
+
+	DB.Where("market_id = ? and status = ?", marketID, common.STATUS_SUCCESSFUL).Order("created_at desc").Find(&trades).Count(&count)
+	return count, trades
+}
+
+func (tradeDaoPG) FindTradesByHash(hash string) []*Trade {
+	var trades []*Trade
+	DB.Where("transaction_hash = ?", hash).Order("created_at desc").Find(&trades)
+	return trades
+}
+
+func (tradeDaoPG) FindTradeByID(id int64) *Trade {
+	var trade Trade
+
+	DB.Where("id = ?", id).Find(&trade)
+	if trade.Status == "" {
+		return nil
+	}
+
+	return &trade
+}
+
+func (tradeDaoPG) FindAccountMarketTrades(account, marketID, status string, limit, offset int) (int64, []*Trade) {
+	var trades []*Trade
+	var count int64
+
+	DB.Where("market_id = ? and (taker = ? or maker = ?)", marketID, account, account).Order("created_at desc").Find(&trades).Count(&count)
+	return count, trades
+}
+
+func (tradeDaoPG) InsertTrade(trade *Trade) error {
+	return DB.Create(trade).Error
+}
+
+func (tradeDaoPG) UpdateTrade(trade *Trade) error {
+	return DB.Save(trade).Error
+}
+
+func (tradeDaoPG) Count() int {
+	var count int
+	DB.Model(&Trade{}).Count(&count)
+	return count
+}
+
+func (tradeDaoPG) FindTradeByTransactionID(transactionID int64) []*Trade {
+	var trades []*Trade
+
+	DB.Where("transaction_id = ? ", transactionID).Order("created_at asc").Find(&trades)
+	return trades
+}
