9e5b87de2d306ba3c16a7e390de0994e7c2f893e
unpopularcoder <unpopularcoder@proton.me>
2024-05-15 21:21:04 +0000

    Update races.md

---
+++ web/src/actions/config.js
@@ -0,0 +1,14 @@
+export const setConfigs = configs => {
+  return {
+    type: 'SET_CONFIGS',
+    payload: configs
+  };
+};
+
+export const loadWeb3NetworkID = web3NetworkID => {
+  web3NetworkID = parseInt(web3NetworkID, 10);
+  return async (dispatch, getState) => {
+    const state = getState();
+    const oldWeb3NetworkID = state.config.get('web3NetworkID');
+
+    if (oldWeb3Netwo
